5a3fc8ffb553cf22bb4deee0a8b566eb139cf1aa
drmorrisontierp <drmorrisontierp@gmail.com>
2024-09-15 16:11:26 +0200

    testing

--- equationtrainer/redoCreateEquation.js
+++ equationtrainer/redoCreateEquation.js
@@ -3,7 +3,7 @@ function createEquation2(level) {
     
     
     
-    let x1 = Math.round(Math.random() * 8 + 1)
+    /*let x1 = Math.round(Math.random() * 8 + 1)
     let x2 = Math.round(Math.random() * 8 + 1)
     let x1b = Math.round(Math.random() * 8 + 1)
     let x2b = Math.round(Math.random() * 8 + 1)
@@ -25,22 +25,42 @@ function createEquation2(level) {
     nox1b = nox1b / g1
     nox2 = nox2 / g2
     nox2b = nox2b / g2
-    
+    */
 
-    let a = Math.round(Math.random() * 8 + 1)*(level > 2 ? (Math.round(Math.random() * 100) < 50 ? 1 : -1) : 1)
-    let o = level < 4 ? 0 : Math.round(Math.random() * 8 + 1)*(Math.round(Math.random() * 100) < 50 ? 1 : -1)
-    let x = a - o
+    let a = Math.round(Math.random() * 8 + 1)*(level > 4 ? (Math.round(Math.random() * 100) < 50 ? 1 : -1) : 1)
+    let o = "1235".includes(level.toString()) ? 0 : Math.round(Math.random() * 8 + 1)*(Math.round(Math.random() * 100) < 50 ? 1 : -1)
  
-    let b = "14".includes(level.toString()) ? 1 : Math.round(Math.random() * 8 + 1)
-    let p = Math.round(Math.random() * 8 + 1)
+    let b = "1456".includes(level.toString()) ? 1 : Math.round(Math.random() * 8 + 1)
+    let p = "12356".includes(level.toString()) ? 1 : Math.round(Math.random() * 8 + 1)
+    
+    let x = (a*p - o*b)/(p*b)
+    //let x = (a*p - o*b)
+    console.log(x)
+    let g1 = gcd(a, b)
+    let g2 = gcd(o, p)
 
-    let f = Math.round(Math.random() * 20 + 1) * x / b
+    a = Math.round(a/g1)
+    b = Math.round(b/g1)
+    o = Math.round(o/g2)
+    p = Math.round(p/g2)
+
+
+    let f = Math.round(Math.random() * 20 + 1) * x * p * b * ("1234".includes(level.toString())? 1 : (Math.round(Math.random() * 100) < 50 ? 1 : -1))
     
     let c = Math.round(Math.random() * 8 + 1) * (Math.round(Math.random() * 100) < 50 ? 1 : -1)
     let d = Math.round(Math.random() * 8 + 1)
+    g1 = gcd(c, d)
+    c = Math.round(c/g1)
+    d = Math.round(d/g1)
     
-    let m = f + c
-    let n = Math.round(Math.random() * 8 + 1)
+    let n = Math.round(Math.random() * 4 + 1)
+    //let m = f + c
+    let m = (f*d*n + c*n)
+    n = n*d
+    g1 = gcd(m, n)
+    m = m/g1
+    n = n/g1
+    
     
     a = a.toString() + "x"
     b = b.toString()
@@ -77,11 +97,10 @@ function createEquation2(level) {
         newArray[9] = ""
     }
     if (level === 3) {
-        let xOneZero = Math.round(Math.random() * 100) < 50 ? true : false;
-        newArray[0] = xOneZero ? a : c;
-        newArray[1] = 1
-        newArray[2] = xOneZero ? (c[0] === "-"? "-" : "+" ) : (a[0] === "-"? "-" : "+" )
-        newArray[3] = xOneZero ? (c[0] === "-"? c.slice(1) : c ) : (a[0] === "-"? a.slice(1) : a ) 
+        newArray[0] = a
+        newArray[1] = b
+        newArray[2] = c[0] === "-"? "-": "+"
+        newArray[3] = c[0] === "-"? c.slice(1): c;
         newArray[4] = 1
         newArray[5] = m
         newArray[6] = 1
@@ -89,263 +108,137 @@ function createEquation2(level) {
         newArray[8] = ""
         newArray[9] = ""
     }
-    console.log(newArray)
-
-    return
-
-
-    while (num2 === 0) {
-        num1 = Math.round(Math.random() * numbers + 1); // Regenerate num1
-        num2 = opp1 === "+" ? numbers + num1 : numbers - num1;
-    }
-
-    while (num4 === 0) {
-        num3 = Math.round(Math.random() * multiple + 1); // Regenerate num1
-        num4 = opp1 === "+" ? (multiple + num3) * (-1) : (num3 - multiple);
-    }
-
-    while (x24 === 0) {
-        x14 = Math.round(Math.random() * x1 + 1)
-        x24 = opp2 === "+" ? (x1 - x14) * (-1) : (x14 - x1)
-    }
-
-    let flip = Math.round(Math.random() * 100) < 50
-    let x114 = `${flip ? x14 : x24}x`
-    let x224 = `${flip ? x24 : x24}x`
-    num1 = num1.toString()
-    num3 = num3.toString()
-    num4 = num4.toString()
-
-    console.log(final, numbers, num1, num2)
-    let xOneNum
-    let xOneZero
-    let xTwoFive
-    let noxOneNum
-    let noxTwoNum
-
-    if (level === 1) {
-        newArray[0] = x1n
-        newArray[1] = 1
-        newArray[2] = opp1
-        newArray[3] = num1
-        newArray[4] = 1
-        newArray[5] = num2
-        newArray[6] = 1
-        newArray[7] = ""
-        newArray[8] = ""
-        newArray[9] = ""
-    }
-    if (level === 2) {
-        xOneZero = Math.round(Math.random() * 100) < 50 ? true : false;
-        numOneMinus = Math.round(Math.random() * 100) < 50 ? true : false;
-        numTwoMinus = Math.round(Math.random() * 100) < 50 ? true : false;
-        newArray[0] = xOneZero ? x1n : opp1 === "+" ? num1 : num1[0] === "-" ? num1.replace("-", "") : "-" + num1
-        newArray[1] = 1
-        newArray[2] = !xOneZero ? opp2 : opp1 === "-" && num1[0] === "-" ? "+" : opp1 === "+" && num1[0] !== "-" ? "+" : "-"
-        newArray[3] = !xOneZero ? x1n : opp1 === "-" && num1[0] === "-" ? num1.replace("-", "") : opp1 === "+" && num1[0] !== "-" ? num1 : num1.replace("-", "")
-        newArray[4] = 1
-        newArray[5] = num2
-        newArray[6] = 1
-        newArray[7] = ""
-        newArray[8] = ""
-        newArray[9] = ""
-    }
-    if (level === 3) {
-        newArray[0] = "x"
-        newArray[1] = x1b
-        newArray[2] = opp1
-        newArray[3] = nox1
-        newArray[4] = 1
-        newArray[5] = nox2
-        newArray[6] = 1
-        newArray[7] = ""
-        newArray[8] = ""
-        newArray[9] = ""
-    }
     if (level === 4) {
-        newArray[0] = x114
+        newArray[0] = a
         newArray[1] = 1
-        newArray[2] = num3[0] === "+" ? "+" : "-"
-        newArray[3] = num3[0] === "+" ? num3 : num3.replace("-", "")
+        newArray[2] = (c[0] === "-"? "-" : "+" ) 
+        newArray[3] = (c[0] === "-"? c.slice(1) : c ) 
         newArray[4] = 1
-        newArray[5] = num4
+        newArray[5] = m
         newArray[6] = 1
-        newArray[7] = x224[0] === "+" ? "+" : "-"
-        newArray[8] = x224[0] === "+" ? x224 : x224.replace("-", "")
+        newArray[7] = (o[0] === "-"? "-" : "+" )
+        newArray[8] = (o[0] === "-"? o.slice(1) : o ) 
         newArray[9] = 1
     }
     if (level === 5) {
-        newArray[0] = x1n
-        newArray[1] = x1d
-        newArray[2] = opp1
-        newArray[3] = nox1
-        newArray[4] = nox1b
-        newArray[5] = nox2
-        newArray[6] = nox2b
-        newArray[7] = opp2
-        newArray[8] = x2n
-        newArray[9] = x2d
-    }
-    if (level === 6) {
-        xOneNum = Math.round(Math.random() * 100) < 50 ? true : false;
-        newArray[0] = xOneNum ? x1n : x1d
-        newArray[1] = xOneNum ? x1d : x1n
-        newArray[2] = opp1
-        newArray[3] = nox1
+        let xPosOne = Math.round(Math.random() * 100) < 50
+        newArray[0] = xPosOne ? a : c
+        newArray[1] = 1
+        newArray[2] = xPosOne ? (c[0] === "-"? "-": "+") : (a[0] === "-"? "-": "+")
+        newArray[3] = xPosOne? (c[0] === "-"? c.slice(1): c) : (a[0] === "-"? a.slice(1): a)
         newArray[4] = 1
-        newArray[5] = nox2
+        newArray[5] = m
         newArray[6] = 1
         newArray[7] = ""
         newArray[8] = ""
         newArray[9] = ""
     }
+    if (level === 6) {
+        let xPosOne = Math.round(Math.random() * 100) < 50
+        let xPosTwo = Math.round(Math.random() * 100) < 50
+        newArray[0] = xPosOne ? a : c
+        newArray[1] = 1
+        newArray[2] = xPosOne ? (c[0] === "-"? "-": "+") : (a[0] === "-"? "-": "+")
+        newArray[3] = xPosOne? (c[0] === "-"? c.slice(1): c) : (a[0] === "-"? a.slice(1): a)
+        newArray[4] = 1
+        newArray[5] = xPosTwo ? m : o
+        newArray[6] = 1
+        newArray[7] = xPosOne ? (o[0] === "-"? "-": "+") : (m[0] === "-"? "-": "+")
+        newArray[8] = xPosOne? (o[0] === "-"? o.slice(1): o) : (m[0] === "-"? m.slice(1): m)
+        newArray[9] = 1
+    }
     if (level === 7) {
-        xOneNum = Math.round(Math.random() * 100) < 50 ? true : false;
-        newArray[0] = xOneNum ? x1n : x1d
-        newArray[1] = xOneNum ? x1d : x1n
-        newArray[2] = opp1
-        newArray[3] = nox1
-        newArray[4] = nox1b
-        newArray[5] = nox2
-        newArray[6] = nox2b
+        newArray[0] = a
+        newArray[1] = b
+        newArray[2] = (c[0] === "-"? "-": "+")
+        newArray[3] = (c[0] === "-"? c.slice(1): c)
+        newArray[4] = d
+        newArray[5] = m
+        newArray[6] = n
+        newArray[7] = (o[0] === "-"? "-": "+")
+        newArray[8] = (o[0] === "-"? o.slice(1): o)
+        newArray[9] = p
+    }
+    if (level === 8) {
+        newArray[0] = b
+        newArray[1] = a
+        newArray[2] = (c[0] === "-"? "-": "+")
+        newArray[3] = (c[0] === "-"? c.slice(1): c)
+        newArray[4] = d
+        newArray[5] = m
+        newArray[6] = n
         newArray[7] = ""
         newArray[8] = ""
         newArray[9] = ""
     }
-
-    if (level === 8) {
-        xOneNum = Math.round(Math.random() * 100) < 50 ? true : false;
-        newArray[0] = xOneNum ? x1n : x1d
-        newArray[1] = xOneNum ? x1d : x1n
-        newArray[2] = opp1
-        newArray[3] = nox1
-        newArray[4] = nox1b
-        newArray[5] = nox2
-        newArray[6] = nox2b
-        newArray[7] = opp2
-        newArray[8] = xOneNum ? x2n : x2d
-        newArray[9] = xOneNum ? x2d : x2n
-    }
     if (level === 9) {
-        xOneNum = Math.round(Math.random() * 100) < 50 ? true : false;
-        noxOneNum = Math.round(Math.random() * 100) < 50 ? true : false;
-        noxTwoNum = Math.round(Math.random() * 100) < 50 ? true : false;
-        xOneZero = Math.round(Math.random() * 100) < 50 ? true : false;
-        xTwoFive = Math.round(Math.random() * 100) < 50 ? true : false;
-        if (xOneNum) {
-            if (noxOneNum) {
-                if (xOneZero) {
-                    newArray[0] = x1n
-                    newArray[1] = x1d
-                    newArray[3] = nox1
-                    newArray[4] = nox1b
-                } else {
-                    newArray[0] = nox1
-                    newArray[1] = nox1b
-                    newArray[3] = x1n
-                    newArray[4] = x1d
-                }
-            } else {
-                if (xOneZero) {
-                    newArray[0] = x1n
-                    newArray[1] = x1d
-                    newArray[3] = nox1b
-                    newArray[4] = nox1
-                } else {
-                    newArray[0] = nox1b
-                    newArray[1] = nox1
-                    newArray[3] = x1n
-                    newArray[4] = x1d
-                }
-            }
-        } else {
-            if (noxOneNum) {
-                if (xOneZero) {
-                    newArray[0] = nox1
-                    newArray[1] = nox1b
-                    newArray[3] = x1d
-                    newArray[4] = x1n
-                } else {
-                    newArray[0] = x1d
-                    newArray[1] = x1n
-                    newArray[3] = nox1
-                    newArray[4] = nox1b
-                }
-            } else {
-                if (xOneZero) {
-                    newArray[0] = x1d
-                    newArray[1] = x1n
-                    newArray[3] = nox1b
-                    newArray[4] = nox1
-                } else {
-                    newArray[0] = nox1b
-                    newArray[1] = nox1
-                    newArray[3] = x1n
-                    newArray[4] = x1d
-                }
-            }
+        let xPosOne = Math.round(Math.random() * 100) < 50
+        let xPosTwo = Math.round(Math.random() * 100) < 50
+        let xNum = Math.round(Math.random() * 100) < 50
+        let a9 = Math.round(Math.random() * 8 + 1)*(Math.round(Math.random() * 100) < 50 ? 1 : -1)
+        let b9 = Math.round(Math.random() * 8 + 1)
+        let c9 = Math.round(Math.random() * 8 + 1)*(Math.round(Math.random() * 100) < 50 ? 1 : -1)
+        let d9 = Math.round(Math.random() * 8 + 1)
+        let m9 = Math.round(Math.random() * 8 + 1)*(Math.round(Math.random() * 100) < 50 ? 1 : -1)
+        let n9 = Math.round(Math.random() * 8 + 1)
+        let o9 = Math.round(Math.random() * 8 + 1)*(Math.round(Math.random() * 100) < 50 ? 1 : -1)
+        let p9 = Math.round(Math.random() * 8 + 1)
+        let g = gcd(a9, b9)
+        a9 = (Math.round(a9/g)).toString() + "x"
+        b9 = (Math.round(b9/g)).toString()
+        g = gcd(c9, d9)
+        c9 = (Math.round(c9/g)).toString()
+        d9 = (Math.round(d9/g)).toString()
+        g = gcd(m9, n9)
+        m9 = (Math.round(m9/g)).toString()
+        n9 = (Math.round(n9/g)).toString()
+        g = gcd(o9, p9)
+        o9 = (Math.round(o9/g)).toString() + "x"
+        p9 = (Math.round(p9/g)).toString()
 
-        }
-        newArray[2] = opp1
-        if (xOneNum) {
-            if (noxTwoNum) {
-                if (xTwoFive) {
-                    newArray[5] = x2n
-                    newArray[6] = x2d
-                    newArray[8] = nox2
-                    newArray[9] = nox2b
-                } else {
-                    newArray[5] = nox2
-                    newArray[6] = nox2b
-                    newArray[8] = x2n
-                    newArray[9] = x2d
-                }
-            } else {
-                if (xTwoFive) {
-                    newArray[5] = x2n
-                    newArray[6] = x2d
-                    newArray[8] = nox2b
-                    newArray[9] = nox2
-                } else {
-                    newArray[5] = nox2b
-                    newArray[6] = nox2
-                    newArray[8] = x2n
-                    newArray[9] = x2d
-                }
-            }
-        } else {
-            if (noxTwoNum) {
-                if (xTwoFive) {
-                    newArray[5] = nox2
-                    newArray[6] = nox2b
-                    newArray[8] = x2d
-                    newArray[9] = x2n
-                } else {
-                    newArray[5] = x2d
-                    newArray[6] = x2n
-                    newArray[8] = nox2
-                    newArray[9] = nox2b
-                }
-            } else {
-                if (xTwoFive) {
-                    newArray[0] = x2d
-                    newArray[1] = x2n
-                    newArray[3] = nox2b
-                    newArray[4] = nox2
-                } else {
-                    newArray[0] = nox2b
-                    newArray[1] = nox2
-                    newArray[3] = x2n
-                    newArray[4] = x2d
-                }
-            }
-
-        }
-        newArray[7] = opp2
+        
+        newArray[0] = xPosOne ? ( xNum ? a9 : b9 ) : c9
+        newArray[1] = xPosOne ? ( xNum ? b9 : a9 ) : d9
+        newArray[2] = xPosOne ? (c9[0] === "-"? "-": "+") : ( a9[0] === "-" ? "-" : "+" )
+        newArray[3] = xPosOne ? (c9[0] === "-"? c9.slice(1): c9) : ( xNum ? (a9[0] === "-" ? a9.slice(1) : a9 ) : b9 )
+        newArray[4] = xPosOne ? d9 : ( xNum ? b9 : (a9[0] === "-" ? a9.slice(1) : a9 ))
+        newArray[5] = xPosTwo ? ( xNum ? o9 : p9 ) : m9
+        newArray[6] = xPosTwo ? ( xNum ? p9 : o9 ) : n9
+        newArray[7] = xPosTwo ? (m9[0] === "-"? "-": "+") : ( o9[0] === "-" ? "-" : "+" )
+        newArray[8] = xPosTwo ? (m9[0] === "-"? m9.slice(1): m9) : ( xNum ? (o9[0] === "-" ? o9.slice(1) : o9 ) : p9 )
+        newArray[9] = xPosTwo ? n9 : ( xNum ? p9 : (o9[0] === "-" ? o9.slice(1) : o9 ))
     }
+    console.log("level: ", level, "NewArray: ", newArray)
+
+    let xIndex = []
+    for (let i = 0; i < newArray.length; i ++) {
+        if (newArray[i].includes("x")) xIndex.push(i)
+        if (xIndex.length > 1) {
+            let xL
+            let xR
+            if (xIndex[0] === 0 || xIndex[0] === 3) {
+                xL = parseFloat((parseInt(newArray[xIndex[0]].replace("x", ""))/parseInt(newArray[xIndex[0] + 1])).toFixed(5))
+                xR = parseFloat((parseInt(newArray[xIndex[1]].replace("x", ""))/parseInt(newArray[xIndex[1] + 1])).toFixed(5))
+            } else {
+                xL = parseFloat((parseInt(newArray[xIndex[0] - 1])/parseInt(newArray[xIndex[0]].replace("x", ""))).toFixed(5))
+                xR = parseFloat((parseInt(newArray[xIndex[1] - 1])/parseInt(newArray[xIndex[1]].replace("x", ""))).toFixed(5))
+            }
+            console.log(xL, xR)
+            if (xL === xR) {
+                console.log("recreating")
+                createEquation2(level) 
+            }
+        }
+        
+    }
+    
+
+    return
+    
 }
 
-createEquation2(2)
+console.log("run")
+createEquation2(9)
 
 
 
